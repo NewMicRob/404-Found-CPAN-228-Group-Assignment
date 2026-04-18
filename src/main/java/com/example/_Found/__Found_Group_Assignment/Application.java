9e3c38982d49951e2cb2ac0f97e1c7a32c9012b8
NewMicRob <microbnew@gmail.com>
2026-04-18 16:58:55 -0400

    added profile to print

--- src/main/java/com/example/_Found/__Found_Group_Assignment/Application.java
+++ src/main/java/com/example/_Found/__Found_Group_Assignment/Application.java
@@ -2,11 +2,16 @@ package com.example._Found.__Found_Group_Assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		String profile = context.getEnvironment().getProperty("spring.profiles.active");
+
+		System.out.println("Profile: " + (profile != null ? profile.toUpperCase() : "DEV (DEFAULT)"));
 	}
 }
